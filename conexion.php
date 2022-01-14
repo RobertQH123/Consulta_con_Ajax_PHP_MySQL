349446c8e702f9f77c2552548dcb4416785d4bc0
Robert Quispe Huacho <82891418+RobertQH123@users.noreply.github.com>
2022-01-13 21:40:07 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -7,6 +7,6 @@ $BD = "prueba";
 $con = mysqli_connect($host,$usuario,$password,$BD);
 
 if(mysqli_connect_errno()){
-  echo "error con la conexion:".mysqli_connect_errno();
+  echo json_encode('error');
 }
 ?>
